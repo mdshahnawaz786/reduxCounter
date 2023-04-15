d37d5e30c9406248be5656b70e5fa3374dc63ae1
mdshahnawaz786 <mdshahnawaz911@gmail.com>
2023-04-16 03:53:28 +0530

    done

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,39 +1,50 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Display = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((storedata) => {
+    return storedata;
+  });
+  console.log(data);
 
-
-    const dispatch = useDispatch()
-    const  data = useSelector((storedata)=>{
-        return storedata
-    })
-    console.log(data);
-    
   return (
-    <div>
-        <h2>{data}</h2>
-        <button onClick={(()=>{
-            dispatch({
-                type: "increment"
-            })
-
-
-
-
-        })}>increment</button>
-        <button  onClick={(()=>{
-            dispatch({
-                type: "decrement"
-            })
-        })}>decrement</button>
-        <button  onClick={(()=>{
-            dispatch({
-                type: "reset"
-            })
-        })}>reset</button>
+   <div  style={{height:"250px", width:"350px",boxShadow:"0 0 10px black", display:"flex",justifyContent:"center",alignItems:"center",margin:"100px auto"}}>
+     <div>
+        <h2>Counter App</h2>
+      <h2>{data}</h2>
+      <div style={{display:"flex",gap:"20px"}}>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "increment",
+          });
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "decrement",
+          });
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "reset",
+          });
+        }}
+      >
+        Reset
+      </button>
+      </div>
     </div>
-  )
-}
+   </div>
+  );
+};
 
-export default Display
+export default Display;
